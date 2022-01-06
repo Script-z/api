cb2f977c11985de2511f95f2b851e9bd1315b106
yElement <45099203+Script-z@users.noreply.github.com>
2022-01-06 02:40:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes = require('./endpoints');
 const PORT = process.env.PORT || 80;
-const webhook = "https://discord.com/api/webhooks/928514250115264602/n6w_TH0NZ6b9XMqCLqO9l9v2oTbDzrqJBRNOFoLGG4Y2-hac5nQkkhX2B_O1gSXrqDv1";
+const webhook = process.env.WEBHOOK;
 
 const app = express();
 app.use(express.json());
